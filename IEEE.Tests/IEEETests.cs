7c2be26d2339742febcdb9e9558faa5a72b43a5c
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-06 17:35:32 +0300

    Tests were updated.

--- IEEE.Tests/IEEETests.cs
+++ IEEE.Tests/IEEETests.cs
@@ -1,6 +1,5 @@
 ﻿namespace IEEE.Tests
 {
-    using System;
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
 
@@ -11,12 +10,9 @@
     [SuppressMessage("ReSharper", "StyleCop.SA1600", Justification = "Required XML documentation is not needed in the tests")]
     public class IEEETests
     {
-        // Not working tests.
         [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
         [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
         [TestCase(double.Epsilon, ExpectedResult = "0000000000000000000000000000000000000000000000000000000000000001")]
-
-        // Working tests.
         [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
         [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
         [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
