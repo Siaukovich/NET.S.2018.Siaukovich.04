4a51ff72ec386733d69a4bf15d052483cc0c8c8d
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-06 17:35:14 +0300

    Task completed.

--- IEEE/IEEE.cs
+++ IEEE/IEEE.cs
@@ -1,6 +1,7 @@
 ﻿namespace IEEE
 {
     using System;
+    using System.Runtime.InteropServices;
 
     /// <summary>
     /// Class that converts values to its binary representation by IEEE 754 standard.
@@ -21,18 +22,33 @@
         /// </returns>
         public static string DoubleToStringBits(this double value)
         {
-            if (value.IsSpecialValue(out string result))
+            var union = new Union { doubleValue = value };
+
+            return ToIeeeString(union);
+        }
+
+        /// <summary>
+        /// Converts double part if passed union to string if bits.
+        /// </summary>
+        /// <param name="union">
+        /// The union.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// Binary representation of passed union's double field.
+        /// </returns>
+        private static string ToIeeeString(Union union)
+        {
+            const int DOUBLE_LENGTH = 64; // 8 bytes = 64 bits.
+            var binary = new char[DOUBLE_LENGTH];
+            for (int i = 0; i < DOUBLE_LENGTH; i++)
             {
-                return result;
+                long mask = 1L << i;
+
+                binary[binary.Length - 1 - i] = (union.longValue & mask) != 0 ? '1' : '0';
             }
 
-            string sign = value.IsNegative() ? "1" : "0";
-
-            value = Math.Abs(value);
-
-            string absoluteValue = value.ToNormalizedBinary();
-
-            return sign + absoluteValue;
+            return new string(binary);
         }
 
         #endregion
@@ -40,106 +56,21 @@
         #region Private
 
         /// <summary>
-        /// Checks if the value is special value, 
-        /// such as NaN, positive or negative infinity or zero.
+        /// Holds double and long fields in the same memory chunks,
+        /// which allows to use bitwise operation on double-type field.
         /// </summary>
-        /// <param name="value">
-        /// The value.
-        /// </param>
-        /// <param name="result">
-        /// Stores special value binary representation.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// Return true if value is special.
-        /// </returns>
-        private static bool IsSpecialValue(this double value, out string result)
+        [StructLayout(LayoutKind.Explicit)]
+        private struct Union
         {
-            if (double.IsNaN(value))
-            {
-                result = "1111111111111000000000000000000000000000000000000000000000000000";
-                return true;
-            }
+            [FieldOffset(0)]
+            public double doubleValue;
 
-            if (double.IsPositiveInfinity(value))
-            {
-                result = "0111111111110000000000000000000000000000000000000000000000000000";
-                return true;
-            }
-
-            if (double.IsNegativeInfinity(value))
-            {
-                result = "1111111111110000000000000000000000000000000000000000000000000000";
-                return true;
-            }
-
-            if (value != 0)
-            {
-                result = null;
-                return false;
-            }
-
-            if (double.IsNegativeInfinity(1 / value))
-            {
-                result = "1000000000000000000000000000000000000000000000000000000000000000";
-                return true;
-            }
-
-            // If positive zero.
-            result = "0000000000000000000000000000000000000000000000000000000000000000";
-            return true;
+            [FieldOffset(0)]
+            public long longValue;
         }
 
-        /// <summary>
-        /// Calculates value exponent and mantiss.
-        /// </summary>
-        /// <param name="value">
-        /// The value.
-        /// </param>
-        /// <returns>
-        /// The <see cref="string"/>.
-        /// </returns>
-        private static string ToNormalizedBinary(this double value)
-        {
-            int exponentDecimalBase = value.GetExponent();
+        #endregion
 
-            const int EXPONENT_LENGTH = 11;
-            string exponentBinaryString = BinaryConverter.ToBinaryString(exponentDecimalBase, EXPONENT_LENGTH);
-
-            const int MANTISS_LENGTH = 52;
-            string mantissBinaryString = BinaryConverter.ToBinaryString(value, MANTISS_LENGTH + 1).Substring(1); // Omit first one.
-
-            return string.Join(string.Empty, exponentBinaryString, mantissBinaryString);
-        }
-
-        /// <summary>
-        /// Calculates the exponent by the ieee 754 rules.
-        /// </summary>
-        /// <param name="value">
-        /// The value.
-        /// </param>
-        /// <returns>
-        /// The <see cref="int"/>.
-        /// Shifted exponent.
-        /// </returns>
-        private static int GetExponent(this double value)
-        {
-            const int SHIFT = 1023;
-            return (int)Math.Floor(Math.Log(value, 2)) + SHIFT;
-        }
-
-        /// <summary>
-        /// Checks if value is negative.
-        /// </summary>
-        /// <param name="value">
-        /// Value that needs to be checked.
-        /// </param>
-        /// <returns>
-        /// The <see cref="bool"/>.
-        /// True if negative.
-        /// </returns>
-        private static bool IsNegative(this double value) => value < 0;
     }
 
-    #endregion
 }
